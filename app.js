ff5f538c38643fa94a23bb9dfa8b662cf077df20
dungptfx13091 <dungptfx13091@funix.edu.vn>
2022-11-20 21:44:37 +0900

    lab4.1

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware!");
-  next();
+app.use("/users", (req, res, next) => {
+  console.log("Route user");
+  res.send("<p>The middleware that handles just/users</p>");
 });
-
-app.use((req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
+app.use("/", (req, res, next) => {
+  console.log("Route default");
+  res.send("<p>The middleware that handles just</p>");
 });
 
 app.listen(3001);
