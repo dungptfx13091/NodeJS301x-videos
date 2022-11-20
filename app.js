4c8cc6f6e6d78c17cb45db452e1e17b10a4f0da8
dungptfx13091 <dungptfx13091@funix.edu.vn>
2022-11-20 20:14:52 +0900

    install express and middleware

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3001);
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3001);
