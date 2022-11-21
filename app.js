403281a9384bb0cd348006f1f720d7d990876bc1
dungptfx13091 <dungptfx13091@funix.edu.vn>
2022-11-21 22:58:58 +0900

    Using express Router

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/users", (req, res, next) => {
-  console.log("Route user");
-  res.send("<p>The middleware that handles just/users</p>");
-});
-app.use("/", (req, res, next) => {
-  console.log("Route default");
-  res.send("<p>The middleware that handles just</p>");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
 
 app.listen(3001);
